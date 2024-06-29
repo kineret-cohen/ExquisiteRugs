53dcb8def3886a041857d9de38b07671c442be8e
kineret-cohen <kineret.cohen972@gmail.com>
2024-06-29 21:32:58 +0300

    added ability to define db name

--- Scripts/inventory_import.py
+++ Scripts/inventory_import.py
@@ -6,10 +6,11 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(levelname)s - InventoryImport - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO, filename='er_activity.log')
 
 class InventoryImport:
-  def __init__(self, host, username, password):
-  	self.host = host
-  	self.username = username
-  	self.password = password
+  def __init__(self, host, database, username, password):
+    self.host = host
+    self.username = username
+    self.database = database
+    self.password = password
 
   def importCSV(self, filename):
   	load_sql = "LOAD DATA LOCAL INFILE '" + filename + "' INTO TABLE er_stage_inventory FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' (item,design,in_stock,in_transit,in_transit_eta,on_loom,on_loom_eta);"
@@ -18,7 +19,7 @@ class InventoryImport:
   		con = pymysql.connect(host=self.host,user=self.username,password=self.password,autocommit=True,local_infile=1)
   		logging.info('Connected to DB: {}'.format(self.host))
 
-  		con.select_db('rugs')
+  		con.select_db(self.database)
 
   		# Create cursor and execute Load SQL
   		cursor = con.cursor()
@@ -47,11 +48,12 @@ if __name__ == "__main__":
   parser.add_argument("-u", "--user_name", required=True, help = "User Name")
   parser.add_argument("-p", "--password", required=True, help = "Password")
   parser.add_argument("-f", "--file", required=True, help = "File Name")
+  parser.add_argument("-db", "--database", default='rugs', required=False, help = "Database Name")
 
   # Read arguments from command line
   args = parser.parse_args()
 
 	# Load CSV to DB
-  inv = InventoryImport(args.host,args.user_name,args.password)
+  inv = InventoryImport(args.host, args.database, args.user_name,args.password)
   inv.importCSV(args.file)
   logging.info('inventory_import completed')
